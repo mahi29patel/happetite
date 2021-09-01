e37b8b8d92a7a88d59f7de48de7f6864f13ad336
Mahipatel29 <mahoo29patel@gmail.com>
2021-09-01 20:04:16 +0530

    Update Intro1.js

--- src/components/Intro1/Intro1.js
+++ src/components/Intro1/Intro1.js
@@ -5,18 +5,21 @@ import lady from '../../images/lady1.jpeg'
 function Intro1() {
   return (
     <div style={{backgroundColor:'#f5edf2', paddingBottom:'60px', paddingTop:'60px'}} id="about">
-    <div className="card" style={{width:'1000px', margin: '0 auto',backgroundColor:'white', boxShadow: 'rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px'}}>
+    <div className="card" style={{width:'1000px', margin: '0 auto', boxShadow: 'rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px'}}>
       <div className="row no-gutters">
-       <div className="col-md-4">
+       <div className="col-md-4" style={{backgroundColor:'transparent !important'}}>
         <img src={lady} width="100%" height="100%"/>
         </div>
-        <div className="col-md-8">
-          <div className="card-body" style={{fontWeight:'semi-bold', margin:'10px',width:'620px', alignContent:'center'}}>
-            <h5 >Dear participants, I welcome you all to this program.<br></br><br></br>
+        <div className="col-md-8" style={{backgroundColor:'white'}}>
+          <div className="card-body" style={{margin:'10px',width:'620px',alignContent:'center', color:'black'}}>
+            <h5 >Dear participants, I welcome you all to this program.
+            <br></br><br></br>
+            <div className="head1" style={{fontWeight:'normal'}}>
         {/* <p className="card-text col-md-10" style={{color:'red',marginLeft:'-30px'}}> */}
             I, Ms Freyana Shinde studying in Amity Institute of Behavioural and Allied Sciences, Mumbai am the developer of the program and have designed this program under the supervision of Dr Rekha Wagani as a part of my PhD thesis.
 Suicidal thoughts have been noted to be a coping mechanism to end psychological and emotional pain felt by a person. Through this program, it is my aim that we should be able to learn how to recognise our negative thinking patterns, how to use effective techniques to deal with stressful situations and in turn reinforce/strengthen them into our daily routine. This program will help you to understand various terms like hope, learned optimism, gratitude, character strengths and positive emotions. 
 This program is designed in such a way that it will use a step by step process and it will enable you to build competence in dealing with suicidal thoughts and increase your well-being. I hope that this program will help all of you and always remember that we are all connected and we affect each other too!
+</div>
 </h5>
             {/* <p className="card-text">
               <small className="text-muted">Card Text 2</small>
